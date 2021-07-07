d1b06c1797c20d80a8f058753d7e957f7afea478
Archianne <helena19w@gmail.com>
2021-07-07 18:58:26 +0100

    title component added

---
+++ src/components/Title/index.js
@@ -0,0 +1,13 @@
+const Title = () => {
+  return (
+    <div className="container">
+      <h1 className="section-heading">
+        <span id="h1span">Fetch</span>
+        <i className="fas fa-utensils"></i>
+        Recipes
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
